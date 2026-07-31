35420f395cf6682e48517acfa319e4f97d839e20
CHINTAGADA HARSHINI <chintagadaharshini7@gmail.com>
2026-07-31 20:43:05 +0530

    Time: 9 ms (94.22%), Space: 48.1 MB (32.08%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int minimumPushes(String word) {
+
+        int[] freq = new int[26];
+
+        // Count frequency of each character
+        for (char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        // Sort frequencies
+        Arrays.sort(freq);
+
+        int ans = 0;
+        int push = 1;
+        int count = 0;
+
+        // Traverse from highest frequency to lowest
+        for (int i = 25; i >= 0; i--) {
+
+            if (freq[i] == 0)
+                break;
+
+            ans += freq[i] * push;
+            count++;
+
+            if (count == 8) {
+                push++;
+                count = 0;
+            }
+        }
+
+        return ans;
+    }
+}
