c1c7a10c7cba932957d720bb20a48c64989842b2
CHINTAGADA HARSHINI <chintagadaharshini7@gmail.com>
2026-08-06 20:11:00 +0530

    Time: 0 ms (100%), Space: 12.4 MB (17.07%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minMoves(self, target, maxDoubles):
+        moves=0
+        while target>1:
+            if maxDoubles==0:
+                return moves+target-1
+            if target%2==0 and maxDoubles>0:
+                target//=2
+                maxDoubles-=1
+            else:
+                target-=1
+            moves+=1
+        return moves
+
+
+        
